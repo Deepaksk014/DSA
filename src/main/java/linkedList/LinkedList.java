4a6278d0ad2cee594120385a40d718416b021b2b
Deepak kumar <deepakkumarsk014@gmail.com>
2025-04-07 12:59:49 +0530

    LinkedList correction

--- src/main/java/linkedList/LinkedList.java
+++ src/main/java/linkedList/LinkedList.java
@@ -22,8 +22,9 @@ public class LinkedList {
      if (head == null){
          head = newNode;
      } else {
-         while (current.next != null)
-             current= current.next;
+         while (current.next != null) {
+             current = current.next;
+         }
          current.next= newNode;
      }
  }
